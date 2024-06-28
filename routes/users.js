83c2b9ce9ec954d8bc0e72696249a932035fddd7
Diallo <mamadouadama609@gmail.com>
2024-06-28 01:29:29 -0500

    added post user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,20 @@
 
 import express from "express";
 
+import { v4 as uuidv4 } from 'uuid';
+//uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
 const router = express.Router();
 
 
 const users = [
-    {
+    {   
+        id: uuidv4(),
         firstName: "Diallo",
         lastName: "Alpha",
         age: 100
     },
-    {
+    {   id: uuidv4(),
         firstName: "Bah",
         lastName: "Madiou",
         age: 100
@@ -25,8 +29,12 @@ router.get('/', (req, res) => {
 
 
 router.post('/', (req, res) => {
-    users.push(req.body)
-    console.log(users);
+
+    const user = req.body
+    // const newUser  = {id:uuidv4(), ...user}
+    // users.push(newUser)
+    users.push({id:uuidv4(), ...user})
+    //console.log(users);
     res.send(users)
 })
 
